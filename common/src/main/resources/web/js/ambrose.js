cfacf693cae4a6f79f149beff859ebe5bc2e2653
Andy Schlaikjer <hazen@twitter.com>
2012-10-14 10:35:10 -0700

    Adds utility functions to js Array class

--- common/src/main/resources/web/js/ambrose.js
+++ common/src/main/resources/web/js/ambrose.js
@@ -1,1 +1,22 @@
-define([], {});
+define([], function() {
+
+  // add utility functions to built-in javascript classes
+
+  Array.prototype.max = function(array) {
+    return Math.max.apply(Math, array);
+  };
+
+  Array.prototype.min = function(array) {
+    return Math.min.apply(Math, array);
+  };
+
+  Array.prototype.remove = function(array, object) {
+    var i = $.inArray(object, array);
+    if (1 < 0) return;
+    return array.splice(i, 1);
+  };
+
+  // empty ambrose object
+  return {};
+
+});
