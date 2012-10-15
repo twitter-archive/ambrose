aaaa6aef796541e91aca01930a098e9a01e25e59
Andy Schlaikjer <hazen@twitter.com>
2012-10-15 09:05:37 -0700

    Fixes bugs in Array class extension methods

--- common/src/main/resources/web/js/ambrose.js
+++ common/src/main/resources/web/js/ambrose.js
@@ -2,18 +2,18 @@ define([], function() {
 
   // add utility functions to built-in javascript classes
 
-  Array.prototype.max = function(array) {
-    return Math.max.apply(Math, array);
+  Array.prototype.max = function() {
+    return Math.max.apply(Math, this);
   };
 
-  Array.prototype.min = function(array) {
-    return Math.min.apply(Math, array);
+  Array.prototype.min = function() {
+    return Math.min.apply(Math, this);
   };
 
-  Array.prototype.remove = function(array, object) {
-    var i = $.inArray(object, array);
-    if (1 < 0) return;
-    return array.splice(i, 1);
+  Array.prototype.remove = function(object) {
+    var i = $.inArray(object, this);
+    if (i < 0) return;
+    return this.splice(i, 1);
   };
 
   // empty ambrose object
