bb1a3a185d9d91401fc8da775cf1c3004b895505
Andy Schlaikjer <hazen@twitter.com>
2013-01-10 21:39:57 -0800

    Updates test page

--- common/src/main/resources/web/js/test.js
+++ common/src/main/resources/web/js/test.js
@@ -11,10 +11,12 @@ require(['jquery', 'ambrose'], function($, Ambrose) {
   $(document).ready(function() {
     console.info('Creating default workflow');
     var workflow = Ambrose.Workflow();
-    var statusMessage = Ambrose.Views.StatusMessage(workflow, $('#ambrose-views-status-message'));
+    //var statusMessage = Ambrose.Views.StatusMessage(workflow, $('#ambrose-views-status-message'));
     var progressBar = Ambrose.Views.ProgressBar(workflow, $('#ambrose-views-progress-bar'));
     var table = Ambrose.Views.Table(workflow, $('#ambrose-views-table'));
     var chord = Ambrose.Views.Chord(workflow, $('#ambrose-views-chord'));
+    //var detail = Ambrose.Views.Detail(workflow, $('#ambrose-views-detail'));
+    var dag = Ambrose.Views.Graph(workflow, $('#ambrose-views-graph'));
     workflow.loadJobs();
     workflow.startEventPolling();
   });
