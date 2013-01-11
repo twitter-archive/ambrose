abe0903dc18d3ee1be861d9970f56c286e57061e
Andy Schlaikjer <hazen@twitter.com>
2013-01-11 01:11:46 -0800

    Updates main.js and workflow.html and removes test resources

--- common/src/main/resources/web/js/main.js
+++ common/src/main/resources/web/js/main.js
@@ -1,23 +1,21 @@
-require([
-  'jquery',
-  'uri',
-  'bootstrap',
-  'ambrose',
-  'ambrose-ui',
-  'ambrose-util',
-  'ambrose-detail',
-  'ambrose-table',
-  'ambrose-chart',
-  'ambrose-chord',
-  'ambrose-dag'
-], function ($, URI, bs, ambrose) {
-  var ui = ambrose.ui();
-  var detail = ambrose.detail(ui);
-  var table = ambrose.table(ui);
-  var chord = ambrose.chord(ui);
-  var dag = ambrose.dag(ui);
+requirejs.config({
+  shim: {
+    'uri': { deps: ['jquery'], exports: 'URI' },
+    'bootstrap': { deps: ['jquery'], exports: 'bootstrap' },
+    'd3': { deps: ['jquery'], exports: 'd3' },
+    'colorbrewer': { deps: [], exports: 'colorbrewer' },
+  }
+});
 
+require(['jquery', 'ambrose'], function($, Ambrose) {
   $(document).ready(function() {
-    ui.load();
+    console.info('Creating default workflow');
+    var workflow = Ambrose.Workflow();
+    var progressBar = Ambrose.Views.ProgressBar(workflow, $('#ambrose-views-progress-bar'));
+    var chord = Ambrose.Views.Chord(workflow, $('#ambrose-views-chord'));
+    var dag = Ambrose.Views.Graph(workflow, $('#ambrose-views-graph'));
+    var table = Ambrose.Views.Table(workflow, $('#ambrose-views-table'));
+    workflow.loadJobs();
+    workflow.startEventPolling();
   });
 });
