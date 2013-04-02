5bc6c9b151d83d974816c44a5d0943fcf967e8de
Andy Schlaikjer <hazen@twitter.com>
2013-04-02 12:47:08 -0700

    Disables chord view till we figure out modularization for view diagrams

--- common/src/main/resources/web/js/main.js
+++ common/src/main/resources/web/js/main.js
@@ -12,25 +12,7 @@ require(['jquery', 'ambrose', 'bootstrap'], function($, Ambrose) {
     console.info('Creating default workflow');
     var workflow = Ambrose.Workflow();
     var progressBar = Ambrose.View.ProgressBar(workflow, $('#ambrose-view-progress-bar'));
-
-    var diagrams = $('#diagrams');
-    var chordContainer = $('#ambrose-view-chord');
-    var graphContainer = $('#ambrose-view-graph');
-
-    var height = diagrams.height();
-    var width = diagrams.width();
-
-    // who needs responsive css?
-    if (width <= 1024) {
-      chordContainer.css('display', 'none');
-      graphContainer.width(width).height(250);
-    } else {
-      chordContainer.width(height);
-      graphContainer.width(width - height);
-    }
-
-    var chord = Ambrose.View.Chord(workflow, chordContainer);
-    var graph = Ambrose.View.Graph(workflow, graphContainer);
+    var graph = Ambrose.View.Graph(workflow, $('#ambrose-view-graph'));
     var table = Ambrose.View.Table(workflow, $('#ambrose-view-table'));
     workflow.loadJobs();
     workflow.startEventPolling();
