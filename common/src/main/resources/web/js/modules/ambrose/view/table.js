009c4c124c0a18ce86502b4f6f10da9877c93a8d
Jiaji Grace Zhang <gzhang@twitter.com>
2013-10-10 12:02:07 -0700

    Tweak the table text: "10 mins, 53 sec" -> "10m 53s".

--- common/src/main/resources/web/js/modules/ambrose/view/table.js
+++ common/src/main/resources/web/js/modules/ambrose/view/table.js
@@ -193,12 +193,12 @@ define(['lib/jquery', 'lib/d3', '../core', './core', 'lib/bootstrap'], function(
           d = Math.floor(h / 24);
           h = h % 24;
 
-          if (d != 0 && d != null) elapsedTime += ", " + d + " days";
-          if (h != 0 && h != null) elapsedTime += ", " + h + " hours";
-          if (m != 0 && m != null) elapsedTime += ", " + m + " mins";
-          if (s != 0) elapsedTime += ", " + s + " sec";
+          if (d != 0) elapsedTime += d + "d ";
+          if (h != 0) elapsedTime += h + "h ";
+          if (m != 0) elapsedTime += m + "m ";
+          if (s != 0) elapsedTime += s + "s ";
 
-          return elapsedTime.substring(2);
+          return elapsedTime;
       };
 
       function pad(number) {
