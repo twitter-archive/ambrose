ef544708db1f20af6ad339a09d2ca7e770638a4b
Aniket Mokashi <amokashi@twitter.com>
2014-04-10 17:13:06 -0700

    better ident added code

--- pig/src/main/java/com/twitter/ambrose/pig/AmbroseUtil.java
+++ pig/src/main/java/com/twitter/ambrose/pig/AmbroseUtil.java
@@ -9,33 +9,33 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.pig.tools.pigstats.PigProgressNotificationListener;
 
 public class AmbroseUtil {
-	
-	protected static Log log = LogFactory.getLog(AmbroseUtil.class);
-	
-	/**
-	 * Uses dynamic proxy to create an instance of PPNL that ignores all the exceptions
-	 * @return new Instance of PPNL that ignores all the exceptions
-	 */
-	public static PigProgressNotificationListener newPPNLWithoutExceptions(final PigProgressNotificationListener ppnl) {
-		return (PigProgressNotificationListener)
-				Proxy.newProxyInstance(
-						AmbroseUtil.class.getClassLoader(), 
-						new Class[] {PigProgressNotificationListener.class}, 
-						new InvocationHandler() {
 
-							@Override
-							public Object invoke(Object proxy, Method method, Object[] args)
-									throws Throwable {
-								try {
-									return method.invoke(ppnl, args);
-								} catch (InvocationTargetException e) {
-									log.warn("Exception while calling " + method.getName() + 
-											" Message:" + e.getTargetException().getLocalizedMessage() + ". Ignoring...");
-									log.debug(e.getTargetException());
-								}
-								return null;
-							}
-						});
-	}
+  protected static Log log = LogFactory.getLog(AmbroseUtil.class);
+
+  /**
+   * Uses dynamic proxy to create an instance of PPNL that ignores all the exceptions
+   * @return new Instance of PPNL that ignores all the exceptions
+   */
+  public static PigProgressNotificationListener newPPNLWithoutExceptions(final PigProgressNotificationListener ppnl) {
+    return (PigProgressNotificationListener)
+        Proxy.newProxyInstance(
+            AmbroseUtil.class.getClassLoader(), 
+            new Class[] {PigProgressNotificationListener.class}, 
+            new InvocationHandler() {
+
+              @Override
+              public Object invoke(Object proxy, Method method, Object[] args)
+                  throws Throwable {
+                try {
+                  return method.invoke(ppnl, args);
+                } catch (InvocationTargetException e) {
+                  log.warn("Exception while calling " + method.getName() + 
+                      " Message:" + e.getTargetException().getLocalizedMessage() + ". Ignoring...");
+                  log.debug(e.getTargetException());
+                }
+                return null;
+              }
+            });
+  }
 
 }
--- pig/src/test/java/com/twitter/ambrose/pig/TestAmbroseUtils.java
+++ pig/src/test/java/com/twitter/ambrose/pig/TestAmbroseUtils.java
@@ -7,60 +7,60 @@ import org.apache.pig.tools.pigstats.PigProgressNotificationListener;
 import org.junit.Test;
 
 public class TestAmbroseUtils {
-	
-	@Test
-	public void testNewPPNLWithoutException() {
-		PigProgressNotificationListener ppnl = 
-				AmbroseUtil.newPPNLWithoutExceptions(new ExceptionThrowingPPNL());
-		ppnl.initialPlanNotification(null, null);
-		ppnl.jobFailedNotification(null, null);
-	}
-	
-	private static class ExceptionThrowingPPNL implements PigProgressNotificationListener {
-		@Override
-		public void initialPlanNotification(String arg0, MROperPlan arg1) {
-			throw new UnsupportedOperationException("NYI");
-			
-		}
 
-		@Override
-		public void jobFailedNotification(String arg0, JobStats arg1) {
-			throw new UnsupportedOperationException("NYI");
-		}
+  @Test
+  public void testNewPPNLWithoutException() {
+    PigProgressNotificationListener ppnl = 
+        AmbroseUtil.newPPNLWithoutExceptions(new ExceptionThrowingPPNL());
+    ppnl.initialPlanNotification(null, null);
+    ppnl.jobFailedNotification(null, null);
+  }
 
-		@Override
-		public void jobFinishedNotification(String arg0, JobStats arg1) {
-			throw new UnsupportedOperationException("NYI");
-		}
+  private static class ExceptionThrowingPPNL implements PigProgressNotificationListener {
+    @Override
+    public void initialPlanNotification(String arg0, MROperPlan arg1) {
+      throw new UnsupportedOperationException("NYI");
 
-		@Override
-		public void jobStartedNotification(String arg0, String arg1) {
-			throw new UnsupportedOperationException("NYI");
-		}
+    }
 
-		@Override
-		public void jobsSubmittedNotification(String arg0, int arg1) {
-			throw new UnsupportedOperationException("NYI");
-		}
+    @Override
+    public void jobFailedNotification(String arg0, JobStats arg1) {
+      throw new UnsupportedOperationException("NYI");
+    }
 
-		@Override
-		public void launchCompletedNotification(String arg0, int arg1) {
-			throw new UnsupportedOperationException("NYI");
-		}
+    @Override
+    public void jobFinishedNotification(String arg0, JobStats arg1) {
+      throw new UnsupportedOperationException("NYI");
+    }
 
-		@Override
-		public void launchStartedNotification(String arg0, int arg1) {
-			throw new UnsupportedOperationException("NYI");
-		}
+    @Override
+    public void jobStartedNotification(String arg0, String arg1) {
+      throw new UnsupportedOperationException("NYI");
+    }
 
-		@Override
-		public void outputCompletedNotification(String arg0, OutputStats arg1) {
-			throw new UnsupportedOperationException("NYI");
-		}
+    @Override
+    public void jobsSubmittedNotification(String arg0, int arg1) {
+      throw new UnsupportedOperationException("NYI");
+    }
 
-		@Override
-		public void progressUpdatedNotification(String arg0, int arg1) {
-			throw new UnsupportedOperationException("NYI");
-		}		
-	}
+    @Override
+    public void launchCompletedNotification(String arg0, int arg1) {
+      throw new UnsupportedOperationException("NYI");
+    }
+
+    @Override
+    public void launchStartedNotification(String arg0, int arg1) {
+      throw new UnsupportedOperationException("NYI");
+    }
+
+    @Override
+    public void outputCompletedNotification(String arg0, OutputStats arg1) {
+      throw new UnsupportedOperationException("NYI");
+    }
+
+    @Override
+    public void progressUpdatedNotification(String arg0, int arg1) {
+      throw new UnsupportedOperationException("NYI");
+    }		
+  }
 }
